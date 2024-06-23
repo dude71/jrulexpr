1b949ebd50e6de2930d00f7362c1aa820ed25ca4
ctevatc <dude71@gmail.com>
2024-06-23 15:39:30 +0200

    minute function optional trigger

--- src/main/java/org/d71/jrulexpr/function/MinuteFunction.java
+++ src/main/java/org/d71/jrulexpr/function/MinuteFunction.java
@@ -3,16 +3,21 @@ package org.d71.jrulexpr.function;
 import com.ezylang.evalex.Expression;
 import com.ezylang.evalex.data.EvaluationValue;
 import com.ezylang.evalex.functions.AbstractFunction;
+import com.ezylang.evalex.functions.FunctionParameter;
 import com.ezylang.evalex.parser.Token;
 import org.d71.jrulexpr.rule.RuleTrigger;
 
 import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@FunctionParameter(name = "trigger", isVarArg = true)
 public class MinuteFunction  extends AbstractFunction implements JrxFunction<BigDecimal> {
+    private boolean trigger = true;
+
     @Override
     public String getToken() {
         return "MINUTE";
@@ -20,11 +25,18 @@ public class MinuteFunction  extends AbstractFunction implements JrxFunction<Big
 
     @Override
     public Set<RuleTrigger> getRuleTriggers() {
-        return Collections.singleton(new RuleTrigger() {
+        return trigger ? Collections.singleton(new RuleTrigger() {
             public String getCronExpression() {
                 return "\"0 * * * * *\"";
             }
-        });
+        }) : Collections.emptySet();
+    }
+
+    @Override
+    public void setParameters(List<Object> values) {
+        if (values.size() > 0) {
+            trigger = (Boolean)values.get(0);
+        }
     }
 
     @Override
